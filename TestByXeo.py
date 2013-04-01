8b7ecb2eeaed7e1e960f0cca04b5e7db2dbf1aa3
Xeomorpher <fwharf.fw@googlemail.com>
2013-04-01 19:39:18 +0100

    Test

--- TestByXeo.py
+++ TestByXeo.py
@@ -1,6 +1,7 @@
 import org.bukkit.Material as Material
 import org.bukkit.inventory.ItemStack as ItemStack
 import org.bukkit.inventory.ShapedRecipe as ShapedRecipe
+import org.bukkit.Bukkit.getServer() as getServer()
 
 R = ShapedRecipe(ItemStack(Material.STICK, 1, 70)).shape("fse", "sss", "asw").setIngredient('f', Material.LAVA_BUCKET).setIngredient('e', Material.DIAMOND).setIngredient('s', Material.STICK).setIngredient('a', Material.FEATHER).setIngredient('w', Material.WATER_BUCKET)
-org.bukkit.Bukkit.getServer().addRecipe(R)
+getServer().addRecipe(R)
