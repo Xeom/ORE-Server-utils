d6684f582695e4fbed71adb2ed2f1af96fd725cf
Xeomorpher <fwharf.fw@googlemail.com>
2013-04-07 20:27:13 +0100

    Plots are cool

--- PlotUtils.py
+++ PlotUtils.py
@@ -15,14 +15,16 @@ def onCommandpwarp(sender, args):
         if len(args) == 2:
             if args[1].isdigit():
                 try:
-                    X = players[args[0]][0][int(args[0])][0]
-                    Z = players[args[0]][0][int(args[0])][1]
+                    C = players[args[0].lower()][0][int(args[1])]
+                    X = C[0]
+                    Z = C[1]
                 except:
                     sender.sendMessage('No such plot')
                     return False
-            else:
-                X = players[args[0]][0][0][0]
-                Z = players[args[0]][0][0][1]
+        else:
+            C = players[args[0]][0][0]
+            X = C[0]
+            Z = C[1]
     else:
         try:
             X = int(args[0], 16)
@@ -32,6 +34,11 @@ def onCommandpwarp(sender, args):
             return False
     X = (X * 256) + 128
     Z = (Z * 256) + 128
+    O = plots[(X,Z)][0]
+    if O != False:
+        sender.sendMessage(''.join(['Teleporting to ',O,"'s plot #",str(plots[(X,Z)][1]),'; ',str(X),',',str(Z)]))
+    else:
+        sender.sendMessage(''.join(['Teleporting to plot ',str(X),',',str(Z),' - Unclaimed']))
     V = Location(sender.getWorld(),X,18,Z)
     sender.teleport(V)
     return True
@@ -42,17 +49,19 @@ def onCommandClaim(sender, args):
         sender.sendMessage(''.join([color('c'),'/claim [X] [Z]']))
         return False
     try:
-        X = int(args[0], 16)
-        Z = int(args[1], 16)
+        X = int(args[0])
+        Z = int(args[1])
     except:
-        sender.sendMessage(''.join([color('c'),'Coords must be two hex values']))
+        sender.sendMessage(''.join([color('c'),'Coords must be two values']))
         return False
     if (X,Z) in plots:
         if plots[(X,Z)][0] == False:
-            n = sender.getName()
+            n = sender.getName().lower()
             if not n in players:
                 players[n] = [[(X,Z)],1]
                 plots[(X,Z)] = [n,1,ctime()]
+                T = ctime().split()
+                mapgen(X,Z,True,[n,'Plot #1','Claimed:',' '.join([T[1],T[2],T[4]])])
                 sender.sendMessage('New user defined, and given a plot')
                 pickle.dump(plots, open("Plots.xeodata", "wb"))
                 pickle.dump(players, open("Players.xeodata", "wb"))
@@ -82,7 +91,9 @@ def onCommandGenerate(sender, args):
         d = int(args[0])
         for x in range(-d,d):
             for z in range(-d,d):
-                plots[(x,z)] = [False,0,'']
+                if plots[(x,z)] == None:
+                    plots[(x,z)] = [False,0,'']
+                    mapgen(x,z,False,['Unclaimed'])
         pickle.dump(plots, open("Plots.xeodata", "wb"))
         sender.sendMessage(''.join(['Generated ',str((d ** 2)*4),'. plots']))
         return True
@@ -92,24 +103,62 @@ def onCommandGiveplot(sender, args):
     if args[0] in players:
         players[args[0]][1] += 1
         pickle.dump(players, open("Players.xeodata", "wb"))
-        sender.sendMessage(''.join([args[0],' now has ',players[args[0]][1],' plots']))
+        sender.sendMessage(''.join([args[0],' now has ',str(players[args[0]][1]),' plots']))
         return True
     sender.sendMessage('Not a valid player')
     return False
 
 @hook.command('unclaimplot', description='Unclaim a plot')
-def onCommandGiveplot(sender, args):
+def onCommandUnclaim(sender, args):
     if len(args) == 2:
         if args[0].isdigit() and args[1].isdigit():
             X = int(args[0])
             Z = int(args[1])
             if (X,Z) in plots:
-                plots[(X,Z)] = [False,0,'']
-                pickle.dump(plots, open("Plots.xeodata", "wb"))
-                return True
+                if plots[(X,Z)] != False:
+                    players[plots[(X,Z)][0]][0].remove((X,Z))
+                    plots[(X,Z)] = [False,0,'']
+                    mapgen(X,Z,False,['Unclaimed'])
+                    pickle.dump(plots, open("Plots.xeodata", "wb"))
+                    pickle.dump(players, open("Players.xeodata", "wb"))
+                    return True
             sender.sendMessage('No such plot')
             return False
         sender.sendMessage('Your coords must be integers')
         return False
     sender.sendMessage('/unclaimplot [X] [Z]')
     return False
+
+@hook.command('psearch', description='Search for a user or plot')
+def onCommandPsearch(sender, args):
+    if len(args) == 0:
+        sender.sendMessage(''.join([color('c'),'/psearch [Criteria]']))
+        return False
+    if args[0].lower() in players:
+        p = players[args[0].lower]
+        for i, v in enumerate(p[0]):
+            sender.sendMessage(''.join(['Plot ',str(i)]))
+            sender.sendMessage(''.join([str(v)]))
+            sender.sendMessage(' '.join(['Claimed',plots[v][2]]))
+        return True
+    if len(args) == 2:
+        if args[0].isdigit() and args[1].isdigit():
+            p = plots[(args[0],args[1])]
+            if p[0] != False:
+                sender.sendMessage(''.join(['Claimed by ',p[0]]))
+                sender.sendMessage(''.join(['At ',p[2]]))
+                return True
+            else:
+                sender.sendMessage('Unclaimed')
+                return True
+    sender.sendMessage(''.join([color('c'),'/psearch [Criteria]']))
+    return False
+
+def mapgen(X,Z,C,T):
+    print 'At X'+str(X)+'and Y'+str(Y)
+    if C:
+        print 'Lit'
+    else:
+        print 'unlit'
+    for I in T
+    print I
